eef758b96533b2c76bb8bbfcd07b51caaa987f18
vaibhav-csfreak <vs12022001@gmail.com>
2025-03-19 18:55:28 +0530

    added EncodeImageToBase64Service in the backend

---
+++ src/main/java/com/vaibhav/portfolio/service/EncodeImageToBase64Service.java
@@ -0,0 +1,28 @@
+package com.vaibhav.portfolio.service;
+
+import org.springframework.stereotype.Service;
+import com.vaibhav.portfolio.model.Projects;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
+
+@Service
+public class EncodeImageToBase64Service {
+    // Helper Method to Encode Image to Base64
+    public String encodeImageToBase64(String filePath) {
+        try {
+            Resource resource = new ClassPathResource(filePath);
+            byte[] imageBytes = Files.readAllBytes(resource.getFile().toPath());
+            return "data:image/png;base64," + Base64.getEncoder().encodeToString(imageBytes);
+        } catch (Exception e) {
+            System.out.println("Error loading image: " + e.getMessage());
+            return "";
+        }
+    }
+}
+
--- src/main/java/com/vaibhav/portfolio/service/ProjectService.java
+++ src/main/java/com/vaibhav/portfolio/service/ProjectService.java
@@ -8,15 +8,24 @@ import java.util.List;
 
 @Service
 public class ProjectService {
+    private final EncodeImageToBase64Service encodeImageToBase64Service;
+
+    public ProjectService(EncodeImageToBase64Service encodeImageToBase64Service) {
+        this.encodeImageToBase64Service = encodeImageToBase64Service;
+    }
+
     public List<Projects> getProjects() {
-        System.out.println("in the service");
+        System.out.println("in the service: "+ encodeImageToBase64Service.encodeImageToBase64("static/projects/project-1.png"));
         return Arrays.asList(
-                new Projects("Crypto Wallet Generator", "https://wallet-app.onrender.com/", "projectImage",
+                new Projects("Crypto Wallet Generator", "https://wallet-app.onrender.com/", encodeImageToBase64Service.encodeImageToBase64("static/projects/project-1.png"),
                         Arrays.asList("Javascript", "EJS", "CSS", "Node.js", "MongoDB")),
-                new Projects("Cloud Storage using Blockchain", "https://github.com/niemajorproject/CloudStorageUsingBlockchain", "projectImage",
+                new Projects("Cloud Storage using Blockchain", "https://github.com/niemajorproject/CloudStorageUsingBlockchain", encodeImageToBase64Service.encodeImageToBase64("../../resources/static/projects/project-2.png"),
                         Arrays.asList("React.js", "Node.js", "Solidity")),
-                new Projects("Movie Site", "https://csfreakmovies.netlify.app/", "projectImage",
+                new Projects("Movie Site", "https://csfreakmovies.netlify.app/", encodeImageToBase64Service.encodeImageToBase64("static/projects/project-3.png"),
                         Arrays.asList("HTML", "CSS", "Javascript"))
                 );
     }
 }
+
+
+// <img src="data:image/jpeg;base64,BASE64_STRING_HERE" alt="Project Image" /> to access in frontend
